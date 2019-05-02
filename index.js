ddd00a353e4feb114347e7ee35b1bf14088f4250
sriley <sarahriley.dev@gmail.com>
2019-05-02 15:48:56 -0400

    changes to deploy to heroku

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const server = require('./server.js');
+require('dotenv').config(); // add this line as the first thing to run1
 
-server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
+const server = require('./api/server.js');
+
+// we'll read the port from the server environment if it is there
+// heroku will have the PORT environment variable set
+const port = process.env.PORT || 5000;
+
+// we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
